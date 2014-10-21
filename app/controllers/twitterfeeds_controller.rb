449f70cb10b7d7356aa75556f1616c23878eb968
sammykhaleel <sammykhaleel83@gmail.com>
2014-10-20 23:28:38 -0400

    twitterfeeds

--- app/controllers/twitterfeeds_controller.rb
+++ app/controllers/twitterfeeds_controller.rb
@@ -1,12 +1,20 @@
 class TwitterfeedsController < ApplicationController
   before_action :set_twitterfeed, only: [:show, :edit, :update, :destroy]
 
+@@client = Twitter::REST::Client.new do |config|
+  config.consumer_key = 'AfmJgIlPWCoetxNUGSVGkQzqN'
+  config.consumer_secret = 'JqZWXepFzApQQ4B1WAd9N5MymHoH1almYvAj95vT7domBbHU5U'
+  config.oauth_token = '1284314953-vnAmfMLM7UGHUOyfWHapkRXXOBoEa5mfLwYpLxP'
+  config.oauth_token_secret = 'XWGJDISQ0TVSdOoV9WNEyAGsGBM1HkqEZdahcD8q4iQk4'
+end
   # GET /twitterfeeds
   # GET /twitterfeeds.json
   def index
-    #@twitter_feed  = Twitter.user_timeline("SammyKhaleel").first.text
+    @tweets = user_timeline(2)#@twitter_feed  = Twitter.user_timeline("SammyKhaleel").first.text
   end
-
+  def user_timeline(qt)
+    @@client.user_timeline(count: qt)
+  end
   # GET /twitterfeeds/1
   # GET /twitterfeeds/1.json
   def show
